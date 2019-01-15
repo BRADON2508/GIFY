3a483085412717b95b313a817c87bb0d7db2a809
Brandon Donahue <mgsbigboss85@live.com>
2019-01-15 17:27:31 -0500

    Commit

--- assets/javascript/gify.js
+++ assets/javascript/gify.js
@@ -1,15 +1,14 @@
 // create variables
 var topics=["marvel", "dc", "magic", "stargate", "jumanji"];
 
-for loop linking variables to the buttons
-for (var i = 0; i < topics.length; i++) { 
-    var buttons = $('<button>'+ topics.text + '</button>') 
+// for loop linking variables to the buttons
+for (var i = 0; i < topics.length; i++) {
+    var buttons = $('<button>'+ topics.text + '</button>')
     buttons.append('topics');
 };
 
 // code to create the input box and submit button
 $form.append('<input type="button" value="button">')
-    
 
 
 // code to append input to new button
@@ -25,10 +24,10 @@ $.ajax({
     console.log(response);
   });
 
-// code to 
+// code to
 
 
-// code to 
+// code to
 //  $.ajax({
 //     url: "https://api.giphy.com/v1/gifs/trending?api_key=Z356ER5WAFzIyQ390uh1yeWvOb3Hg8f6",
 //     method: "GET"
@@ -43,4 +42,4 @@ $.ajax({
     console.log(response);
   });
 
-$(document).on("click", function(){console.log("response")}) 
+$(document).on("click", function(){console.log("response")});
